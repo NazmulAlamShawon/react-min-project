e211fbd81186093c98f790eca89097dda9079cd0
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-13 19:01:40 +0600

    add logins setup

--- thememode/src/App.jsx
+++ thememode/src/App.jsx
@@ -1,12 +1,13 @@
 
+import UserContextProvider from "./context/UserContextProvider";
 function App() {
  
 
   return (
-    <>
+    <UserContextProvider>
     <h1 className="text-3xl font-bold underline"> react with context api</h1>
   
-    </>
+    </UserContextProvider>
   )
 }
 
