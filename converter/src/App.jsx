38d612421f8737f1d4b412f4ce0641ab50825e58
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-13 15:57:12 +0600

    add swap

--- converter/src/App.jsx
+++ converter/src/App.jsx
@@ -1,6 +1,23 @@
 import {useState} from 'react'
+
+import InputBox from "./components";
+
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 const App = () => {
-  const [count,setCount] =useState(0)
+  const [amount,setAmount] =useState(0)
+  const [from,setFrom] = useState("usd")
+  const [to,setTo] = useState("inr")
+  const [convertAmount,setConvertAmount] = useState(0)
+  const currencyInfo= useCurrencyInfo(from)
+    Object.key(currencyInfo)
+    
+    const swap = () => {
+      setFrom(to)
+      setTo(from)
+      setConvertAmount(amount)
+      setAmount(convertAmount)
+    }
+
   return (
     <div>
        <h1 className="text-center">Currency app</h1>
